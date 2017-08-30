a24d7b26c6698c0eba36ffe6bf236b85c9abc91c
e-y-e <e_y_e@tuta.io>
2017-08-30 20:33:15 +0100

    Add high-level example to package.d

--- source/yql/package.d
+++ source/yql/package.d
@@ -18,3 +18,27 @@ module yql;
 
 public import yql.query;
 public import yql.response;
+
+/**
+ * Analyzing tech corporations by market capitalization.
+ */
+@safe @nogc pure nothrow
+unittest
+{
+    import std.algorithm.searching : all;
+
+    static immutable data = [column("Name"), column("MarketCapitalization"), column("Ask"), column("Bid"), column("Change")];
+
+    static immutable stocks = [value("'AAPL'"), value("'GOOG'"), value("'YHOO'"), value("'FB'"), value("'AMZN'")];
+    assert(stocks.all!(v => v.str != "null"));
+
+    immutable query = table("yahoo.finance.quotes")
+        .select(data)
+        .where(column("symbol").among(stocks))
+        .orderBy(column("MarketCapitalization"), Order.descending);
+    assert(query.valid);
+
+//     immutable uri = query
+//         .range
+//         .uri;
+}
